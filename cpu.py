549b37ac22e507e2482a41e682c698446bb32cd2
ChaseSmith67 <smitcha6@oregonstate.edu>
2023-03-26 04:06:13 -0500

    add getter and setter methods for registers

--- cpu.py
+++ cpu.py
@@ -5,12 +5,17 @@ import numpy as np
 
 
 class CPU(object):
+    """
+    Represents the Central Processing Unit, designed to emulate the 6052
+    Microprocessor.
+    """
 
     def __init__(self):
+        """Initializes the CPU object and sets all registers and flags to 0."""
         # 8-Bit Registers: A, X & Y
-        self.reg_A = np.uint8(0)    # Accumulator
-        self.reg_X = np.uint8(0)    # X Index
-        self.reg_Y = np.uint8(0)    # Y Index
+        self.reg_A = np.uint8(0)    # Accumulator Register
+        self.reg_X = np.uint8(0)    # Index Register X
+        self.reg_Y = np.uint8(0)    # Index Register Y
 
         # TODO: consider implementing these as an array?
         # P-Register - 1-Bit Flags
@@ -27,3 +32,42 @@ class CPU(object):
 
         # 16-Bit Program Counter
         self.counter = np.uint16(0)
+
+    def get_reg_A(self) -> np.uint8:
+        """Return the 8-bit value stored in Accumulator Register"""
+        return self.reg_A
+
+    def set_reg_A(self, val: np.uint8) -> None:
+        """Stores the given value in the Accumulator Register"""
+        self.reg_A = val
+
+    def get_reg_X(self) -> np.uint8:
+        """Return the 8-bit value stored in Index Register X"""
+        return self.reg_X
+
+    def set_reg_X(self, val: np.uint8) -> None:
+        """Stores the given value in Index Register X"""
+        self.reg_X = val
+
+    def get_reg_Y(self) -> np.uint8:
+        """Return the 8-bit value stored in Index Register Y"""
+        return self.reg_Y
+
+    def set_reg_Y(self, val: np.uint8) -> None:
+        """Stores the given value in Index Register Y"""
+        self.reg_Y = val
+
+    def get_stack_pointer(self) -> np.uint8:
+        """Returns the position of the stack pointer"""
+
+    def set_stack_pointer(self, pos: np.uint8) -> None:
+        """Moves the stack pointer to the specified position"""
+        self.pointer = pos
+
+    def get_counter(self) -> np.uint16:
+        """Returns the value of the Program Counter"""
+        return self.counter
+
+    def set_counter(self, val: np.uint16) -> None:
+        """Sets the Program Counter to the specified value"""
+        self.counter = val
