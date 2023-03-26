a7586e6079537c0b8a4af675e1c1e96c6d9a6413
ChaseSmith67 <smitcha6@oregonstate.edu>
2023-03-26 05:45:44 -0500

    create instructions file

---
+++ instructions.py
@@ -0,0 +1,23 @@
+# Author: Chase Smith
+# GitHub: ChaseSmith67
+
+# This dictionary will store the functions responsible for carrying out the
+# specified actions. "None" placeholders will remain until they're written
+
+instructions = {
+    "ADC": None, "AND": None, "ASL": None, "BCC": None,
+    "BCS": None, "BEQ": None, "BIT": None, "BMI": None,
+    "BNE": None, "BPL": None, "BRK": None, "BVC": None,
+    "BVS": None, "CLC": None, "CLD": None, "CLI": None,
+    "CLV": None, "CMP": None, "CPX": None, "CPY": None,
+    "DEC": None, "DEX": None, "DEY": None, "EOR": None,
+    "INC": None, "INX": None, "INY": None, "JMP": None,
+    "JSR": None, "LDA": None, "LDX": None, "LDY": None,
+    "LSR": None, "NOP": None, "ORA": None, "PHA": None,
+    "PHP": None, "PLA": None, "PLP": None, "ROL": None,
+    "ROR": None, "RTI": None, "RTS": None, "SBC": None,
+    "SEC": None, "SED": None, "SEI": None, "STA": None,
+    "STX": None, "STY": None, "TAX": None, "TAY": None,
+    "TSX": None, "TXA": None, "TXS": None, "TYA": None
+}
+
