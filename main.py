875f412ee863296eaf7ab477d88fb714f4faa6c1
ChaseSmith67 <smitcha6@oregonstate.edu>
2023-03-26 00:21:22 -0500

    add ROMs folder add gitignore

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import argparse
 def parse_args() -> object:
     parser = argparse.ArgumentParser()
     parser.add_argument("--path", "-p", type=str, help="Path to ROM file")
+    # TODO: set up default ROM path, so user only enters ROM title
 
     args = parser.parse_args()
 
@@ -16,10 +17,15 @@ def parse_args() -> object:
 
 def main():
     args = parse_args()
+
+    # TODO: validate ROM path
     rom_path = args.path
 
     print(rom_path)
 
+    with open(rom_path, "rb") as file:
+        for line in file.readlines():
+            print(line)
 
 
 if __name__ == "__main__":
