6e3edc3c25470dff2105fa7acc5c8fe30dafaa18
ChaseSmith67 <smitcha6@oregonstate.edu>
2023-03-26 03:33:18 -0500

    import numpy for memory optimization

--- cpu.py
+++ cpu.py
@@ -1,27 +1,29 @@
 # Author: Chase Smith
 # GitHub: ChaseSmith67
 
+import numpy as np
+
 
 class CPU(object):
 
     def __init__(self):
         # 8-Bit Registers: A, X & Y
-        self.reg_A = None   # Accumulator
-        self.reg_X = None   # X Index
-        self.reg_Y = None   # Y Index
+        self.reg_A = np.uint8(0)    # Accumulator
+        self.reg_X = np.uint8(0)    # X Index
+        self.reg_Y = np.uint8(0)    # Y Index
 
         # TODO: consider implementing these as an array?
         # P-Register - 1-Bit Flags
-        self.flag_N = None
-        self.flag_V = None
-        self.flag_B = None
-        self.flag_D = None
-        self.flag_I = None
-        self.flag_Z = None
-        self.flag_C = None
+        self.flag_N = np.bool(0)
+        self.flag_V = np.bool(0)
+        self.flag_B = np.bool(0)
+        self.flag_D = np.bool(0)
+        self.flag_I = np.bool(0)
+        self.flag_Z = np.bool(0)
+        self.flag_C = np.bool(0)
 
         # 8-Bit Stack Pointer
-        self.pointer = None
+        self.pointer = np.uint8(0)
 
         # 16-Bit Program Counter
-        self.counter = None
+        self.counter = np.uint16(0)
